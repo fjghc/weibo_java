1ee4a3d32521dbce5a55aa557bc014147eb6d9b3
lizherui <lzrak47m4a1@gmail.com>
2013-03-13 19:36:30 +0800

    first commit

---
+++ dev/weibo4j/dev/timeline/GetUserTimeline.java
@@ -0,0 +1,25 @@
+package weibo4j.dev.timeline;
+
+import weibo4j.Timeline;
+import weibo4j.model.Status;
+import weibo4j.model.StatusWapper;
+import weibo4j.model.Paging;
+import weibo4j.model.WeiboException;
+
+public class GetUserTimeline {
+
+	public static void main(String[] args) {
+		String access_token = args[0];
+		Timeline tm = new Timeline();
+		tm.client.setToken(access_token);
+		try {
+			StatusWapper status = tm.getUserTimelineByUid(args[1]);
+			for(Status s : status.getStatuses()){
+				System.out.println(s.getText());
+			}
+		} catch (WeiboException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
